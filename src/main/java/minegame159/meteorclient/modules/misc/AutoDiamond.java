182130d29cdc9181511ccf4c0c384f9dfd0c4cfd
spasskopf <unconfigured@null.spigotmc.org>
2021-02-13 10:57:56 +0100

    cleaned up AutoDiamond

--- src/main/java/minegame159/meteorclient/modules/misc/AutoDiamond.java
+++ src/main/java/minegame159/meteorclient/modules/misc/AutoDiamond.java
@@ -1,28 +1,24 @@
 package minegame159.meteorclient.modules.misc;
 
 import baritone.api.BaritoneAPI;
-import baritone.api.pathing.goals.GoalGetToBlock;
-import baritone.api.pathing.goals.GoalXZ;
 import meteordevelopment.orbit.EventHandler;
 import minegame159.meteorclient.events.world.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
+import minegame159.meteorclient.utils.player.ChatUtils;
 import net.minecraft.util.math.BlockPos;
-import org.lwjgl.system.CallbackI;
 
 import java.io.File;
-import java.io.FileReader;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AutoDiamond extends Module {
     public AutoDiamond() {
-        super(Category.Misc, "auto-diamond", "Automatically trades with The World for Diamonds");
+        super(Category.Misc, "auto-diamond", "Automatically farms diamond, which have been saved to a file");
     }
 
     ArrayList<BlockPos> pos;
-    int cooldown = 0;
     int index = 0;
     boolean exception = true;
     BlockPos current;
@@ -30,39 +26,43 @@ public class AutoDiamond extends Module {
     @Override
     public void onActivate() {
         try {
-            System.out.println("Activates");
             List<String> data = load("C:\\Users\\Martin\\Desktop\\Diamanten\\Markus-Server\\info.txt");
             pos = getPos(data.get(0));
             index = Integer.parseInt(data.get(1));
             exception = false;
         } catch (Exception e) {
             System.out.println("Damage ERROR");
+            toggle();
             exception = true;
         }
     }
 
     @Override
     public void onDeactivate() {
-        System.out.println("Deactivates");
+        ChatUtils.info("Deactivated Auto-Diamond :(");
     }
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        if (!exception) {
-            if (BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().getGoal() == null) {
-                System.out.println("Index: " + index + ", Pos: " + pos.get(index).toString());
-                current = pos.get(index);
-                BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().execute("goto " + current.getX() + " " + current.getY() + " " + current.getZ());
-                index++;
-                if (index >= pos.size()) exception = true;
+        if (isActive()) {
+            if (!exception) {
+                if (BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().getGoal() == null) {
+                    System.out.println("Index: " + index + ", Pos: " + pos.get(index).toString());
+                    current = pos.get(index);
+                    BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().execute(String.format("goto %d %d %d", current.getX(), current.getY(), current.getZ()));
+                    index++;
+                    if (index >= pos.size()) {
+                        exception = true;
+                    }
+                }
             }
         }
     }
 
     public ArrayList<BlockPos> getPos(String path) {
         List<String> in = load(path);
-        ArrayList<BlockPos> out = new ArrayList<BlockPos>();
-        String a[];
+        ArrayList<BlockPos> out = new ArrayList<>();
+        String[] a;
         for (int i = 0; i < in.size(); i++) {
             a = in.get(i).split(" ");
             out.add(new BlockPos(Integer.parseInt(a[2]), Integer.parseInt(a[3]), Integer.parseInt(a[4].substring(0, a[4].length() - 1))));
@@ -75,6 +75,8 @@ public class AutoDiamond extends Module {
             File file = new File(path);
             return Files.readAllLines(file.toPath());
         } catch (Exception e) {
+            ChatUtils.error("Could not load file! Stopping!");
+            toggle();
         }
         return null;
     }
