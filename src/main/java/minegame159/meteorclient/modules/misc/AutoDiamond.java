7b60260e6cec66752885e499b4146d3aa71a1a04
spasskopf <unconfigured@null.spigotmc.org>
2021-02-13 14:26:56 +0100

    improved AutoDiamond: Search for blocks or mine them! Added a few settings

--- src/main/java/minegame159/meteorclient/modules/misc/AutoDiamond.java
+++ src/main/java/minegame159/meteorclient/modules/misc/AutoDiamond.java
@@ -3,21 +3,24 @@ package minegame159.meteorclient.modules.misc;
 import baritone.api.BaritoneAPI;
 import baritone.api.pathing.goals.GoalBlock;
 import meteordevelopment.orbit.EventHandler;
+import minegame159.meteorclient.MeteorClient;
 import minegame159.meteorclient.events.world.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
-import minegame159.meteorclient.settings.BoolSetting;
-import minegame159.meteorclient.settings.Setting;
-import minegame159.meteorclient.settings.SettingGroup;
-import minegame159.meteorclient.settings.StringSetting;
+import minegame159.meteorclient.settings.*;
 import minegame159.meteorclient.utils.player.ChatUtils;
+import net.minecraft.block.Block;
 import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.math.BlockPos;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class AutoDiamond extends Module {
@@ -35,59 +38,124 @@ public class AutoDiamond extends Module {
     private State state;
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgMining = settings.createGroup("Mine Diamonds");
+    private final SettingGroup sgFinding = settings.createGroup("Find Diamonds");
 
-    private final Setting<String> path = sgGeneral.add(new StringSetting.Builder()
-            .name("path")
-            .description("the path to the file.")
-            .defaultValue("info.txt")
+    private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+            .name("mode")
+            .description("Search for blocks or mine blocks you found before.")
+            .defaultValue(Mode.SEARCH_FOR_BLOCKS)
             .build()
     );
 
-    public final Setting<Boolean> autoWalkHome = sgGeneral.add(new BoolSetting.Builder()
+    private final Setting<String> path = sgMining.add(new StringSetting.Builder()
+            .name("path")
+            .description("the path to the file.")
+            .defaultValue(new File(MeteorClient.FOLDER, "info.txt").getAbsolutePath())
+            .build()
+    );
+
+    public final Setting<Boolean> autoWalkHome = sgMining.add(new BoolSetting.Builder()
             .name("walk-home")
             .description("Will walk 'home' when you mined everything.")
             .defaultValue(false)
             .build());
 
-    public final Setting<Boolean> autoLogOut = sgGeneral.add(new BoolSetting.Builder()
+    public final Setting<Boolean> autoLogOut = sgMining.add(new BoolSetting.Builder()
             .name("log-out-on")
             .description("Logs out when you mined everything.")
             .defaultValue(false)
             .build());
 
+    public final Setting<Integer> minHeight = sgFinding.add(new IntSetting.Builder()
+            .name("min-height")
+            .description("Minimum Height for searching")
+            .defaultValue(6)
+            .min(0)
+            .max(256)
+            .sliderMax(256)
+            .build());
+
+    public final Setting<Integer> maxHeight = sgFinding.add(new IntSetting.Builder()
+            .name("max-height")
+            .description("Maximum Height for searching")
+            .defaultValue(6)
+            .min(0)
+            .max(256)
+            .sliderMax(256)
+            .build()
+    );
+
+    public final Setting<Integer> radius = sgFinding.add(new IntSetting.Builder()
+            .name("radius")
+            .description("Radius for searching")
+            .defaultValue(128)
+            .min(0)
+            .max(512)
+            .sliderMax(512)
+            .build()
+    );
+
+    private final Setting<List<Block>> blocks = sgFinding.add(new BlockListSetting.Builder()
+            .name("blocks")
+            .description("Blocks to search for.")
+            .defaultValue(new ArrayList<>(0))
+            .build()
+    );
+    private final Setting<String> output = sgFinding.add(new StringSetting.Builder()
+            .name("output")
+            .description("Location of the saved file.")
+            .defaultValue(new File(MeteorClient.FOLDER, "info.txt").getAbsolutePath())
+            .build()
+    );
+
+
     @Override
     public void onActivate() {
-        if (mc.player != null) {
-            playerX = (int) mc.player.getX();
-            playerY = (int) mc.player.getY();
-            playerZ = (int) mc.player.getZ();
-        }
-        try {
-            List<String> data = load(path.get());
-            pos = getPos(data.get(0));
-            index = Integer.parseInt(data.get(1));
-            exception = false;
-        } catch (Exception e) {
-            e.printStackTrace();
-            toggle(false);
-            exception = true;
-
+        if (mode.get() == Mode.BREAK_BLOCKS) {
+            if (mc.player != null) {
+                playerX = (int) mc.player.getX();
+                playerY = (int) mc.player.getY();
+                playerZ = (int) mc.player.getZ();
+            }
+            try {
+                List<String> data = load(path.get());
+                pos = getPos(data.get(0));
+                index = Integer.parseInt(data.get(1));
+                exception = false;
+            } catch (Exception e) {
+                e.printStackTrace();
+                toggle(false);
+                exception = true;
+            }
+        } else {
+            try {
+                startSearching();
+            } catch (IOException e) {
+                ChatUtils.error("An unexpected error occurred!");
+                ChatUtils.error(e.getMessage());
+                e.printStackTrace();
+            } finally {
+                toggle(false);
+            }
         }
     }
 
     @Override
     public void onDeactivate() {
         ChatUtils.info("Deactivated Auto-Diamond :(");
-        BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().execute("cancel");
-        state = State.IDLE;
-        index = 0;
-        exception = false;
+        if (mode.get() == Mode.BREAK_BLOCKS) {
+            BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().execute("cancel");
+            state = State.IDLE;
+            index = 0;
+            exception = false;
+        }
     }
 
     @EventHandler
     private void onTick(TickEvent.Post event) {
 
-        if (isActive()) {
+        if (isActive() && mode.get() == Mode.BREAK_BLOCKS) {
             if (!exception) {
 
                 if (BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().getGoal() == null) {
@@ -109,6 +177,72 @@ public class AutoDiamond extends Module {
             }
         }
 
+    }
+
+    int found = 0, currentBlock = 0, blocksToScan = 0;
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private void startSearching() throws IOException {
+        if (mc.player == null) {
+            ChatUtils.error("Player is null. Disabling!");
+            return;
+        }
+        if (mc.player.world == null) {
+            ChatUtils.error("Player-World is null. Disabling!");
+            return;
+        }
+        if (maxHeight.get() < minHeight.get()) {
+            ChatUtils.error("Max height must not be less than Min height!");
+            return;
+        }
+
+        File file = new File(MeteorClient.FOLDER, "AutoDiamond_" + new SimpleDateFormat("yy_MM_dd_hh_mm").format(new Date()));
+        File infoFile = new File(output.get());
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        if (!infoFile.exists()) {
+            infoFile.createNewFile();
+        }
+        FileWriter infoWriter = new FileWriter(infoFile);
+        infoWriter.write(file.getAbsolutePath());
+        infoWriter.write("\n0");
+        infoWriter.flush();
+        infoWriter.close();
+        ChatUtils.info("File has been saved to %s (use this file's path as  AutoDiamond input)!", infoFile.getAbsolutePath());
+
+        FileWriter writer = new FileWriter(file);
+
+        final int x = (int) mc.player.getPos().getX();
+        final int z = (int) mc.player.getPos().getZ();
+        final StringBuilder blocksFound = new StringBuilder();
+        blocksToScan = radius.get() * radius.get() * (maxHeight.get() - minHeight.get());
+        found = 0;
+        currentBlock = 0;
+        for (int xPos = x - radius.get(); xPos < x + radius.get(); xPos++) {
+            for (int zPos = z - radius.get(); zPos < z + radius.get(); zPos++) {
+                for (int height = minHeight.get(); height < maxHeight.get(); height++) {
+                    BlockPos blockPos = new BlockPos(xPos, height, zPos);
+                    currentBlock++;
+                    if (blocks.get().contains(mc.player.world.getBlockState(blockPos).getBlock())) {
+                        found++;
+                        blocksFound.append("[.b goto ")
+                                .append(blockPos.getX())
+                                .append(" ")
+                                .append(blockPos.getY())
+                                .append(" ")
+                                .append(blockPos.getZ())
+                                .append("]\n");
+
+                    }
+                }
+            }
+        }
+        writer.write(blocksFound.toString());
+        writer.flush();
+        writer.close();
+        ChatUtils.info("Finished Searching! File with all blocks: " + file.getAbsolutePath());
+        toggle(false);
     }
 
     private void stop() {
@@ -135,14 +269,6 @@ public class AutoDiamond extends Module {
         }
     }
 
-    @Override
-    public String getInfoString() {
-        if (pos == null || index >= pos.size()) {
-            return "Idle";
-        }
-        return String.format("%s, %d%%", pos.get(index).toShortString(), index * 100 / pos.size());
-    }
-
     public ArrayList<BlockPos> getPos(String path) {
         List<String> in = load(path);
         ArrayList<BlockPos> out = new ArrayList<>();
@@ -152,6 +278,25 @@ public class AutoDiamond extends Module {
             out.add(new BlockPos(Integer.parseInt(a[2]), Integer.parseInt(a[3]), Integer.parseInt(a[4].substring(0, a[4].length() - 1))));
         }
         return out;
+    }
+
+    @Override
+    public String getInfoString() {
+        if (mode.get() == Mode.BREAK_BLOCKS) {
+
+            if (pos == null || index >= pos.size()) {
+                return "Idle";
+            }
+            return String.format("%s, %d%%", pos.get(index).toShortString(), index * 100 / pos.size());
+        } else {
+            return String.format("Searching %d/%d (%s%%), Found: %d (%s%%)",
+                    currentBlock,
+                    blocksToScan,
+                    blocksToScan == 0 ? "NaN" : (currentBlock * 100) / ((float) blocksToScan),
+                    found,
+                    currentBlock == 0 ? "NaN" : (found * 100) / ((float) currentBlock)
+            );
+        }
     }
 
     public List<String> load(String path) {
@@ -182,4 +327,17 @@ public class AutoDiamond extends Module {
         }
     }
 
+    public enum Mode {
+        SEARCH_FOR_BLOCKS, BREAK_BLOCKS;
+
+        @Override
+        public String toString() {
+            final String name = this.name();
+            return (name.charAt(0) +
+                    name.toLowerCase()
+                            .substring(1))
+                    .replace("_", " ");
+        }
+
+    }
 }
